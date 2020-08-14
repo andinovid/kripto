12cf4ae384536917ced28da5727a4e273900fc9f
Andi Novid <andi.novid@gmail.com>
2020-08-14 22:16:01 +0700

    fix google login

--- application/config/google_config.php
+++ application/config/google_config.php
@@ -2,4 +2,4 @@
 
 $config['google_client_id'] = "832750725613-n9u37al68kj3k0j2ho04862gevocumk1.apps.googleusercontent.com";
 $config['google_client_secret'] = "AX6JTOiBisW2L9Ndf0x7ope0";
-$config['google_redirect_url'] = base_url() . 'auth/oauth2callback';
+$config['google_redirect_url'] = base_url() . 'login/oauth2callback';
